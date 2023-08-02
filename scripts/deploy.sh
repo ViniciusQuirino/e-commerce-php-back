e6dab7806da499c834b0103a7c265256d7f5a3f5
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-08-02 16:01:55 -0300

    chore: bug fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,3 @@ php artisan route:cache
 
 echo "Running migration"
 php artisan migrate --force
-
-echo "Starting queue worker in the background"
-# nohup php artisan queue:work database --daemon --tries=5 &
-
-# Adicione um atraso (opcional) para permitir que o worker inicie completamente
-# sleep 5
-
-# Opcional: Exibir uma mensagem de conclusão do deploy
-# echo "Deployment completed!"
